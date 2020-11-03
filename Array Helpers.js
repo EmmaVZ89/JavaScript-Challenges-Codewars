e57e596490668afc6d56497913c62db729ac30a7
Emmanuel Zelarayan <emmavz@live.com>
2020-11-03 10:30:05 -0300

    Solution

--- Array Helpers.js
+++ Array Helpers.js
@@ -19,3 +19,16 @@
 // numbers.sum();     // must return 15
 // numbers.even();    // must return [2, 4]
 // numbers.odd();     // must return [1, 3, 5]
+
+
+Array.prototype.square = function() {return this.map(num => num*num)};
+
+Array.prototype.cube = function() {return this.map(num => num*num*num)};
+
+Array.prototype.sum = function() {return this.reduce((a,b) => a+b, 0)};
+
+Array.prototype.average = function() {return this.sum() / this.length};
+
+Array.prototype.even = function() {return this.filter((num) => num%2 === 0)};
+
+Array.prototype.odd = function() {return this.filter((num) => num%2 !== 0)};
