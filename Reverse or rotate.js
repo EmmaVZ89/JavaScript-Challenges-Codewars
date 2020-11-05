4e06b179e19ab87a8c6be943dc95678773efbc8c
Emmanuel Zelarayan <emmavz@live.com>
2020-11-05 17:36:19 -0300

    add Reverse or rotate

---
+++ Reverse or rotate.js
@@ -0,0 +1,45 @@
+// The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+//
+// If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+//
+// If
+//
+// sz is <= 0 or if str is empty return ""
+// sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+// Examples:
+// revrot("123456987654", 6) --> "234561876549"
+// revrot("123456987653", 6) --> "234561356789"
+// revrot("66443875", 4) --> "44668753"
+// revrot("66443875", 8) --> "64438756"
+// revrot("664438769", 8) --> "67834466"
+// revrot("123456779", 8) --> "23456771"
+// revrot("", 8) --> ""
+// revrot("123456779", 0) --> ""
+// revrot("563000655734469485", 4) --> "0365065073456944"
+
+
+function revrot(str, sz) {
+
+  let arrayNumbers = str.split("");
+  let chunk;
+  let cubeAndSum;
+  let result = [];
+
+    if (str === "" || sz <= 0 || sz > str.length) {
+      return "";
+    }
+
+  for (let i = 0; i < Math.floor(str.length/sz); i++ ) {
+    chunk = arrayNumbers.splice(0, sz);
+    cubeAndSum = chunk.map(item => parseInt(item)**3).reduce((a,b) => a+b, 0);
+    if (cubeAndSum % 2 === 0) {
+      result.push(chunk.reverse());
+    } else {
+      let rotateOnePosition = chunk.shift();
+      chunk.push(rotateOnePosition);
+      result.push(chunk);
+    }
+  }
+
+  return result.map(item => item.join("")).join("");
+}
