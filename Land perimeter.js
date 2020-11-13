419aeb2da419c39dfe572a5b0c690bdbac5891a2
Emmanuel Zelarayan <emmavz@live.com>
2020-11-13 11:56:33 -0300

    add Land perimeter

---
+++ Land perimeter.js
@@ -0,0 +1,43 @@
+// Task:
+// Given an array arr of strings complete the function landPerimeter by calculating the total perimeter of all the islands. Each piece of land will be marked with 'X' while the water fields are represented as 'O'. Consider each tile being a perfect 1 x 1piece of land. Some examples for better visualization:
+//
+// ['XOOXO',
+//  'XOOXO',
+//  'OOOXO',
+//  'XXOXO',
+//  'OXOOO']
+// or
+//
+//
+// should return: "Total land perimeter: 24",
+// while
+//
+//
+// ['XOOO',
+//  'XOXO',
+//  'XOXO',
+//  'OOXX',
+//  'OOOO']
+//
+// should return: "Total land perimeter: 18"
+//
+// Good luck!
+
+
+function landPerimeter(arr) {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[0].length; j++) {
+      if (arr[i][j] == 'O') continue;
+      result+=4;
+      if (i!=arr.length-1 && arr[i+1][j]!='O') {
+         result-=2;
+        }
+      if (j!=arr[0].length-1 && arr[i][j+1]!='O') {
+         result-=2;
+        }
+    }
+  }
+  return 'Total land perimeter: ' + result;
+}
