201e94fc09149708582a4b1bc59a1203af958968
Emmanuel Zelarayan <emmavz@live.com>
2020-11-14 13:05:53 -0300

    Solution

--- Math Issues.js
+++ Math Issues.js
@@ -5,3 +5,16 @@
 // Math.round()
 // Math.ceil()
 // Math.floor()
+
+
+Math.round = function(number) {
+  return (number - parseInt(number) >= 0.5) ? parseInt(number) + 1 : parseInt(number) ;
+};
+
+Math.ceil = function(number) {
+  return (parseInt(number) === number) ? number : parseInt(number) + 1;
+};
+
+Math.floor = function(number) {
+  return parseInt(number);
+};
